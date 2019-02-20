32304712b0049c9866d9ab4cae79372cde718332
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-02-20 14:04:50 +0700

    Update to v0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='podder-task-base',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     author="podder-ai",
     url='https://github.com/podder-ai/podder-task-base',
