19414f3c0455fb44efb96be192d9b5b0ba7986a4
Keisuke Minami <lastshogun13@gmail.com>
2019-03-11 17:47:28 +0700

    glocal valiable to clas valiable

--- podder_task_base/log/log_setting.py
+++ podder_task_base/log/log_setting.py
@@ -6,15 +6,12 @@ import yaml
 
 class LogSetting:
     LOG_YML_PATH = 'log.yml'
-
-    def __init__(self):
-        pass
+    _log_setting = None
 
     def load(self):
-        global _log_setting
-        if _log_setting is None:
-            _log_setting = self._load_log_yml()
-        return(_log_setting)
+        if LogSetting._log_setting is None:
+            LogSetting._log_setting = self._load_log_yml()
+        return(LogSetting._log_setting)
 
     def _load_log_yml(self):
         file_path = self.LOG_YML_PATH
@@ -22,7 +19,7 @@ class LogSetting:
             with open(file_path, 'r') as stream:
                 ret = yaml.load(stream)
         else:
-            # load defalut values
+            # if "log.yml" not found, load defalut values
             ret = {
                 "task_name": "task-name-sample",
                 "task_log_format": "%(asctime)s:%(levelname)s:%(message)s",
@@ -30,7 +27,4 @@ class LogSetting:
                 "sql_log_format": "%(asctime)s:%(levelname)s:%(message)s",
                 "sql_log_level": logging.INFO,
             }
-
         return ret
-
-_log_setting = None
