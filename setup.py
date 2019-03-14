562f4c48b4b25220eb78073df1ef98da95849cda
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-03-14 13:58:05 +0700

    Update version to v0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = ['grpcio-tools==1.18.0',
 
 setup(
     name='podder-task-base',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author="podder-ai",
     url='https://github.com/podder-ai/podder-task-base',
