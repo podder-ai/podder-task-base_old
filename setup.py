7fca0619ae41a39a644e8dcd911594bd9f6f2ab2
Keisuke Minami <lastshogun13@gmail.com>
2019-03-22 20:47:06 +0700

    PA-236/task-initializer

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ install_requires = ['grpcio-tools==1.18.0',
                     'mysqlclient==1.4.1',
                     'SQLAlchemy==1.2.17',
                     'python-dotenv==0.10.1',
-#                    'click==6.7',
-#                    'jinja2==2.10',
+                    'click==6.7',
+                    'jinja2==2.10',
                     'PyYAML==5.1',
                     'pytest==4.3.1']
 
@@ -19,4 +19,14 @@ setup(
     url='https://github.com/podder-ai/podder-task-base',
     include_package_data=True,
     install_requires=install_requires,
+    package_data = {
+#        'podder_task_base': ['*.txt', '*.sh'],
+        'podder_task_base': [
+            'task_initializer/templates/*',
+            'task_initializer/templates/api/*',
+            'task_initializer/templates/api/protos/*', 
+            'task_initializer/templates/scripts/*',
+        ],
+    },
+
 )
