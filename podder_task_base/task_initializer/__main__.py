06f7695d0bef5d1bb66d1aadb01033d377a518dc
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-05-16 20:33:14 +0700

    Stop installing podder_lib

--- podder_task_base/task_initializer/__main__.py
+++ podder_task_base/task_initializer/__main__.py
@@ -5,9 +5,6 @@ import click
 
 from podder_task_base import __version__
 from podder_task_base.task_initializer.builder import Builder
-from podder_task_base.task_initializer.podder_lib_install import PodderLibInstall
-from podder_task_base.exceptions import DownloadUrlNotFoundError
-
 
 # CLI codes.
 # Add '-h' option
@@ -23,18 +20,11 @@ def task_initializer():
 # [python -m podder_task_base.task_initializer init] command
 @click.command()
 @click.argument('task-name')
-@click.option('--download-url',
-    required=True,
-    help="download url for podder-lib")
 @click.option('--target-dir',
-    default='/usr/local/poc_base',
-    help="install directory")
-def init(task_name: str, download_url: str, target_dir: str):
-    try:
-        PodderLibInstall(download_url).execute()
-        Builder(task_name, target_dir).init_task()
-    except DownloadUrlNotFoundError:
-        pass
+              default='/usr/local/poc_base',
+              help="install directory")
+def init(task_name: str, target_dir: str):
+    Builder(task_name, target_dir).init_task()
 
 
 task_initializer.add_command(init)
